60120b2f0ed5957ba13d27822a60daf97bc50a6f
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2025-06-12 09:08:44 -0500

    added dispatch fxn to TaskForm

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,6 +1,56 @@
-function TaskForm(){
-  return (
-    <></>
+import { useState, useEffect } from 'react';
+import { FaPlusSquare, FaEdit } from 'react-icon/fa';
+
+function TaskForm({ dispatch, editingTask, setEditingTask }){
+  const[taskText, setTaskText] = useState('');
+
+  useEffect(() => {
+    if (editingTask){
+      setTaskText(editingTask.text);
+    } else{
+      setTaskText('');
+    }
+  }, [editingTask]);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (!taskText.trim()) return;
+
+    const currentDate = new Date().toLocaleString();
+
+    if (editingTask){
+      dispatch({
+        type: 'UPDATE_TASK', 
+        payload: {id: editingTask.id, text: taskText, date: currentDate}
+      });
+      setEditingTask(null);
+    } else {
+      dispatch({
+        type: 'ADD_TASK',
+        payload: {id: Date.now(), text: taskText, completed: false, date: currentDate}
+      });
+    }
+    setTaskText('');
+  }
+  return(
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <input 
+          type='text'
+          value={taskText}
+          onChange=''
+          placeholder=''
+          className=''
+        />
+        <button
+          type='submit'
+          className=''
+        >
+          {editingTask ? <FaEdit size={20} /> : <FaPlusSquare size={20} />}
+          {editingTask ? 'Update Task' : 'Add Task'} 
+        </button>
+      </form>
+    </>
   );
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,25 +1,9 @@
-function TaskList({ dispatch, editingTask, setEditingTask }) {
-  const[taskText, setTaskText] = useState('');
-  return(
+function TaskList() {
+  return (
     <>
-      <form>
-        <input 
-          type='text'
-          value={taskText}
-          onChange=''
-          placeholder=''
-          className=''
-        />
-        <button
-          type='submit'
-          className=''
-        >
-          {editingTask ? <Edit size={20} /> : <PlusCircle size={20} />}
-          {editingTask ? 'Update Task' : 'Add Task'} 
-        </button>
-      </form>
+
     </>
-  );
+  )
 }
 
 export default TaskList;
